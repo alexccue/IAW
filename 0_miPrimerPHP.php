cb23135c2ffd7769edf1675fb9e40ef31350e0f2
alexccue <117975603+alexccue@users.noreply.github.com>
2022-11-18 10:01:34 +0100

    HolaMundo
    Esto es un hola mundo

---
+++ 0_miPrimerPHP.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejemplo</title>
+    </head>
+    <body>
+
+        <?php
+        //IMPORTANTE... QUE ES ESTO 
+        
+        var_dump 
+        echo "¡Hola, soy un script de PHP!<br>";
+        echo "---------------------------<br>";
+        echo $_SERVER['HTTP_USER_AGENT']."<br>"; 
+        echo "---------------------------<br>";
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'Edg') !== FALSE) {
+            echo 'Está usando Microsoft Edge.<br />';
+        }
+        ?>
+    </body>
+</html>
